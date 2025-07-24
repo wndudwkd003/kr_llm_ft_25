248b2165c538966a7b1121c5e86284528b4eb8b8
wndudwkd003 <ymail3@naver.com>
2025-07-24 07:24:08 +0000

    update sf config and complete sft trainer

--- src/configs/lora_config.py
+++ src/configs/lora_config.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass, field
 class LoRAConfig:
     r: int = 128
     lora_alpha: int = 256
-    lora_dropout: float = 0.2
+    lora_dropout: int|float = 0
     target_modules: str|list = field(default_factory=lambda: ["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"])
     bias: str = "none"
     init_lora_weights: str = "xavier_uniform"
